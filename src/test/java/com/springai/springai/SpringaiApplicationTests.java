9a5d15f6b543f335568e33f2ca50506b92db2b40
miharyjoel <miharyjoel@gmail.com>
2024-01-06 22:00:01 +0300

    feat: init project

---
+++ src/test/java/com/springai/springai/SpringaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
