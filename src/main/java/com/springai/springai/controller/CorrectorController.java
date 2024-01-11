b281c5ed206a8f144a04d178ad429ec0dd6e7ad0
miharyjoel <miharyjoel@gmail.com>
2024-01-11 17:13:50 +0300

    feat: add system prompt

--- src/main/java/com/springai/springai/controller/CorrectorController.java
+++ src/main/java/com/springai/springai/controller/CorrectorController.java
@@ -4,9 +4,12 @@ import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.Generation;
 import org.springframework.ai.prompt.Prompt;
 import org.springframework.ai.prompt.PromptTemplate;
+import org.springframework.ai.prompt.SystemPromptTemplate;
+import org.springframework.ai.prompt.messages.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,8 +19,15 @@ public class CorrectorController {
   ChatClient chatClient;
   @GetMapping("/corrector")
   public Generation translator(@RequestBody String sentences){
+    String systemPrompt = """
+                You are a helpful AI assistant that helps people Correct the spelling and grammar mistakes.
+                You should reply to the user's request in the style of a professional.
+                """;
+    SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemPrompt);
+    Message systemMessage = systemPromptTemplate.createMessage();
     PromptTemplate promptTemplate = new PromptTemplate("Correct the spelling and grammar mistakes in the given  sentences : {sentences}");
-    Prompt prompt = promptTemplate.create(Map.of( "sentences", sentences));
+    Message userMessage = promptTemplate.createMessage(Map.of("sentences", sentences));
+    Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
     return chatClient.generate(prompt).getGeneration();
   }
 }
--- src/main/java/com/springai/springai/controller/TranslatorController.java
+++ src/main/java/com/springai/springai/controller/TranslatorController.java
@@ -1,13 +1,18 @@
 package com.springai.springai.controller;
 
 
+
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.Generation;
 import org.springframework.ai.prompt.Prompt;
 import org.springframework.ai.prompt.PromptTemplate;
+import org.springframework.ai.prompt.SystemPromptTemplate;
+import org.springframework.ai.prompt.messages.Message;
+import org.springframework.ai.prompt.messages.MessageType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,8 +22,16 @@ public class TranslatorController {
   ChatClient chatClient;
     @GetMapping("/translator")
     public Generation translator(@RequestParam(required = true) String translate, @RequestBody String sentences){
+      String systemPrompt = """
+                You are a helpful AI assistant that helps people translate given text to %s.
+                You should reply to the user's request in the style of a professional.
+                """.formatted(translate);
+      SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(systemPrompt);
+      Message systemMessage = systemPromptTemplate.createMessage();
       PromptTemplate promptTemplate = new PromptTemplate("translate  into {translate} the given  sentences : {sentences}");
-      Prompt prompt = promptTemplate.create(Map.of("translate", translate, "sentences", sentences));
+      Message userMessage = promptTemplate.createMessage(Map.of("translate", translate, "sentences", sentences));
+      Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
       return chatClient.generate(prompt).getGeneration();
     }
 }
+
